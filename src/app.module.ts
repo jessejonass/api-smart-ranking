7f5d2bf5345786d00baf29505907528e669f7043
Jesse Jonas <jessejonas13@gmail.com>
2022-06-08 10:52:41 -0300

    feat: added mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    PlayersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@nestjs-api-smart-rankin.l80zm.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [],
 })
 export class AppModule {}
