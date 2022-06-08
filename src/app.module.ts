d31f23865e2779c2e1d5cc422da2dd897781e6ec
Jesse Jonas <jessejonas13@gmail.com>
2022-06-08 20:57:32 -0300

    feat: added CategoriesModel to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { PlayersModule } from './players/players.module';
     MongooseModule.forRoot(
       'mongodb+srv://admin:admin@nestjs-api-smart-rankin.l80zm.mongodb.net/?retryWrites=true&w=majority',
     ),
+    CategoriesModule,
   ],
   controllers: [],
 })
