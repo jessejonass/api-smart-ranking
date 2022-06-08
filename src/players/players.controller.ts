4b32f0688e2d47ceb20d7cb8719d5c588d5e29b5
Jesse Jonas <jessejonas13@gmail.com>
2022-06-08 15:38:23 -0300

    refactor: refactored players controller

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -3,13 +3,15 @@ import {
   Controller,
   Delete,
   Get,
-  Patch,
+  Param,
   Post,
-  Query,
+  Put,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
+import { UpdatePlayerDto } from './dtos/update-player.dto';
+import { Player } from './entities/Player';
 import { PlayersValidationParamsPipe } from './pipes/players-validation-params.pipe';
 import { PlayersService } from './players.service';
 
@@ -17,34 +19,36 @@ import { PlayersService } from './players.service';
 export class PlayersController {
   constructor(private readonly playersService: PlayersService) {}
 
-  @Post('create') // api/v1/players/create
-  @UsePipes(ValidationPipe)
-  async create(@Body() player: CreatePlayerDto) {
-    this.playersService.create(player);
-    return player;
+  @Post() // route
+  @UsePipes(ValidationPipe) // dto must have pipe
+  async create(@Body() createPlayerDto: CreatePlayerDto): Promise<Player> {
+    return this.playersService.create(createPlayerDto);
   }
 
-  @Patch('update')
+  @Get()
+  async findAll(): Promise<Player[]> {
+    return await this.playersService.findAll();
+  }
+
+  @Get(':_id')
+  async findOne(
+    @Param('_id', PlayersValidationParamsPipe) _id: string,
+  ): Promise<Player> {
+    return await this.playersService.findOne(_id);
+  }
+
+  @Put(':_id')
   async update(
-    @Query('email', PlayersValidationParamsPipe) email: string,
-    @Body() player: CreatePlayerDto,
-  ) {
-    return this.playersService.update(email, player);
+    @Param('_id', PlayersValidationParamsPipe) _id: string,
+    @Body() updatePlayerDto: UpdatePlayerDto,
+  ): Promise<void> {
+    await this.playersService.update(_id, updatePlayerDto);
   }
 
-  @Delete('delete')
-  async delete(@Query('email', PlayersValidationParamsPipe) email: string) {
-    console.log('email', email);
-    return this.playersService.delete(email);
-  }
-
-  @Get('all')
-  async findAll() {
-    return this.playersService.findAll();
-  }
-
-  @Get('find')
-  async findOne(@Query('email', PlayersValidationParamsPipe) email: string) {
-    return this.playersService.findOne(email);
+  @Delete(':_id')
+  async delete(
+    @Param('_id', PlayersValidationParamsPipe) _id: string,
+  ): Promise<void> {
+    await this.playersService.delete(_id);
   }
 }
