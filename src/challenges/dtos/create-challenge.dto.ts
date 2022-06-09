4428211ae9da85f88af3b6c07af0e9379b295055
Jesse Jonas <jessejonas13@gmail.com>
2022-06-09 11:58:13 -0300

    feat: created create-challenge dto

---
+++ src/challenges/dtos/create-challenge.dto.ts
@@ -0,0 +1,22 @@
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsArray,
+  IsDateString,
+  IsNotEmpty,
+} from 'class-validator';
+import { Player } from 'src/players/entities/Player';
+
+export class CreateChallengeDto {
+  @IsNotEmpty()
+  @IsDateString()
+  challengeDatetime: Date;
+
+  @IsNotEmpty()
+  requester: Player;
+
+  @IsArray()
+  @ArrayMinSize(2)
+  @ArrayMaxSize(2)
+  players: Player[];
+}
