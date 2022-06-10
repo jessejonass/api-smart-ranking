0d82e81bcaa72cf9a14a3b0ed916d2e25f4c5c7b
Jesse Jonas <jessejonas13@gmail.com>
2022-06-10 14:31:33 -0300

    feat: added challenges module

--- src/challenges/challenges.module.ts
+++ src/challenges/challenges.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from 'src/categories/categories.module';
+import { PlayersModule } from 'src/players/players.module';
 import { ChallengesController } from './challenges.controller';
 import { ChallengesService } from './challenges.service';
 import { ChallengeSchema } from './entities/Challenge.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Player', schema: ChallengeSchema }]),
+    MongooseModule.forFeature([{ name: 'Challenge', schema: ChallengeSchema }]),
+    PlayersModule,
+    CategoriesModule,
   ],
   controllers: [ChallengesController],
   providers: [ChallengesService],
