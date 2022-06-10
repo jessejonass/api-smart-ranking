fa38724106737af4e14d7a18419cede6d8de4f0c
Jesse Jonas <jessejonas13@gmail.com>
2022-06-10 14:32:11 -0300

    feat: added create challenge service and controller

--- src/challenges/challenges.controller.ts
+++ src/challenges/challenges.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ChallengesService } from './challenges.service';
+import { CreateChallengeDto } from './dtos/create-challenge.dto';
+import { Challenge } from './entities/Challenge';
 
-@Controller('challenges')
-export class ChallengesController {}
+@Controller('api/v1/challenges')
+export class ChallengesController {
+  constructor(private readonly challengesService: ChallengesService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async create(
+    @Body() createChallengeDto: CreateChallengeDto,
+  ): Promise<Challenge> {
+    return this.challengesService.create(createChallengeDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Challenge[]> {
+    return await this.challengesService.findAll();
+  }
+}
--- src/challenges/challenges.service.ts
+++ src/challenges/challenges.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CategoriesService } from 'src/categories/categories.service';
+import { PlayersService } from 'src/players/players.service';
+import { CreateChallengeDto } from './dtos/create-challenge.dto';
+import { Challenge, ChallengStatus } from './entities/Challenge';
 
 @Injectable()
-export class ChallengesService {}
+export class ChallengesService {
+  constructor(
+    @InjectModel('Challenge') private readonly challengeModel: Model<Challenge>,
+    private readonly playersService: PlayersService,
+    private readonly categoriesService: CategoriesService,
+  ) {}
+
+  async create(createChallengeDto: CreateChallengeDto): Promise<Challenge> {
+    // check requester player exists
+    await this.playersService.findOne(createChallengeDto.requester._id);
+
+    // check requester player is challenged
+    const requesterIsRequested = createChallengeDto.players.some(
+      (player) => player._id === createChallengeDto.requester._id,
+    );
+
+    if (!requesterIsRequested)
+      throw new BadRequestException('Requester must be in the challenge');
+
+    // check requested players exists
+    const players = await this.playersService.findAll();
+    createChallengeDto.players.map((player) => {
+      const playerFilter = players.some(
+        (p) => String(p._id) === String(player._id),
+      );
+      if (!playerFilter)
+        throw new BadRequestException(`Player: ${player._id} does not exist`);
+    });
+
+    // check player requester has category
+    const { category } = await this.categoriesService.findPlayerCategory(
+      createChallengeDto.requester._id,
+    );
+
+    const challenge = {
+      ...createChallengeDto,
+      category,
+      challengeRequestDatetime: new Date(),
+      status: ChallengStatus.PENDING,
+    };
+
+    const newChallenge = new this.challengeModel(challenge);
+    return await newChallenge.save();
+  }
+
+  async findAll(): Promise<Challenge[]> {
+    return await this.challengeModel.find().exec();
+  }
+}
