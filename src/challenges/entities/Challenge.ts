979b6a1eb640ce6f772fda65add72df43873a005
Jesse Jonas <jessejonas13@gmail.com>
2022-06-13 12:28:17 -0300

    refactor: export Match type

--- src/challenges/entities/Challenge.ts
+++ src/challenges/entities/Challenge.ts
@@ -1,7 +1,7 @@
 import { Document } from 'mongoose';
 import { Player } from 'src/players/entities/Player';
 import { ChallengeStatusEnum } from './ChallengeStatus.enum';
-import { Result } from './Result';
+import { Match } from './Match';
 
 export class Challenge extends Document {
   challengeDatetime: Date;
@@ -10,13 +10,6 @@ export class Challenge extends Document {
   challengeResponseDatetime: Date;
   requester: Player;
   category: string;
-  player: Player[];
+  players: Player[];
   match: Match[];
 }
-
-export type Match = {
-  category: string;
-  def: Player;
-  players: Player[];
-  result: Result;
-};
